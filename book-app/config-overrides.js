7adff7d126e1ee59e4ab1e388bd5ef79ab4e60fd
Maksim Beliavsky <maksimbell503@gmail.com>
2023-07-04 12:08:26 +0300

    fix aliases

--- book-app/config-overrides.js
+++ book-app/config-overrides.js
@@ -8,11 +8,11 @@ module.exports = function override(config) {
         '@features': 'src/features',
         '@pages': 'src/pages',
         '@shared': 'src/shared',
-        '@assets': 'src/assets',
         '@constants': 'src/constants',
         '@components': 'src/components',
         '@api': 'src/api',
         '@routes': 'src/routes',
+        '@assets': 'src/assets',
     })(config);
 
     return config;
